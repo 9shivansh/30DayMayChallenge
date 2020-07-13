98043d85d9faa28cd5e077524a271dd8be671686
Shivansh <9shivanshsharma@gmail.com>
2020-07-13 22:23:04 +0530

    Add files via upload

--- SameTree.cpp
+++ SameTree.cpp
@@ -11,12 +11,24 @@
  */
 class Solution {
 public:
-     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q) return true;
-       if (!p && q) return false;
-        if (p && !q) return false;
-        return (p->val == q->val) && 
-            isSameTree(p->left, q->left) &&
-            isSameTree(p->right, q->right);
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        if(p == NULL && q == NULL)
+        {
+            return true;
+        }
+        
+        if(p == NULL || q == NULL)
+        {
+            return false;
+        }
+        
+        if(p -> val != q -> val)
+        {
+            return false;
+        }
+        
+        return p -> val == q -> val && isSameTree(p -> left, q -> left) && isSameTree(p -> right, q -> right);
+        
     }
 };
